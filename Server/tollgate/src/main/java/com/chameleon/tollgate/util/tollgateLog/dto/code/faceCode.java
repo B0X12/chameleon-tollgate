4688bc7a77b706fd7bc557f2213bad01fb094c85
SeongYun <tjddbs2065@gmail.com>
2021-07-22 21:24:40 +0900

    0702 log and history last commit

---
+++ Server/tollgate/src/main/java/com/chameleon/tollgate/util/tollgateLog/dto/code/faceCode.java
@@ -0,0 +1,24 @@
+package com.chameleon.tollgate.util.tollgateLog.dto.code;
+
+public enum faceCode implements LogCode {
+	NO_FACE("No Face", 1),
+	WRONG_HASH("Wrong Hash", 2);
+
+	private final String log;
+	private final int code;
+	private faceCode(String log, int code) {
+		this.log = log;
+		this.code = code;
+	}
+	
+	@Override
+	public int getErrorCode() {
+		return code;
+	}
+	
+	@Override
+	public String getErrorMessage() {
+		return log;
+	}
+
+}
---
+++ Server/tollgate/src/main/java/com/chameleon/tollgate/util/userHistory/UserHistory.java
@@ -0,0 +1,34 @@
+package com.chameleon.tollgate.util.userHistory;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.chameleon.tollgate.util.userHistory.dao.HistoryDAO;
+import com.chameleon.tollgate.util.userHistory.dto.HistoryFactor;
+import com.chameleon.tollgate.util.userHistory.dto.HistoryResult;
+import com.chameleon.tollgate.util.userHistory.dto.historyRecord;
+
+@Service
+public class UserHistory {
+	@Autowired
+	HistoryDAO dao;
+	
+	public boolean leave(String id, HistoryFactor factor, String pc, HistoryResult result) throws Exception {		
+		dao.open();
+		boolean daoResult = dao.record(id, factor.getFactor(), pc, result.getCode());
+		dao.commit();
+		dao.close();
+		
+		return daoResult;
+	}
+	
+	public ArrayList<historyRecord> get(String id) throws Exception {		
+		dao.open(true);
+		ArrayList<historyRecord> recordList = dao.get(id);
+		dao.close();
+		
+		return recordList;
+	}
+}
