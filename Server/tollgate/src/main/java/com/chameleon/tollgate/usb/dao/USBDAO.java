35e0279f9e0383ae361feb709ed614f574bfb382
Hanjun Lim <bjj7399@naver.com>
2021-07-22 20:55:53 +0900

    Modify USBDAO

--- Server/tollgate/src/main/java/com/chameleon/tollgate/usb/dao/USBDAO.java
+++ Server/tollgate/src/main/java/com/chameleon/tollgate/usb/dao/USBDAO.java
@@ -9,10 +9,6 @@ import java.util.List;
 import org.springframework.stereotype.Component;
 import com.chameleon.tollgate.database.SQLiteManager;
 import com.chameleon.tollgate.usb.dto.USBInfo;
-<<<<<<< HEAD
-=======
-
->>>>>>> 51cf37ac16f4b1362ef8a6f6f23438bb1f9d8496
 
 @Component
 public class USBDAO extends SQLiteManager implements IUSBDAO {
@@ -38,7 +34,6 @@ public class USBDAO extends SQLiteManager implements IUSBDAO {
 		 * usb_info="USBVID_0781PID_55674C530001151013119203";
 		 */
 		int Result = 0;
-<<<<<<< HEAD
 
 		String SqlInstruct = "SELECT COUNT(*) FROM auth_usb WHERE id='" + user + "' AND usb_id='" + usb_info + "'";
 		Result = SearchQuery_int(SqlInstruct);
@@ -96,64 +91,6 @@ public class USBDAO extends SQLiteManager implements IUSBDAO {
 	}
 
 	@Override
-=======
-
-		String SqlInstruct = "SELECT COUNT(*) FROM auth_usb WHERE id='" + user + "' AND usb_id='" + usb_info + "'";
-		Result = SearchQuery_int(SqlInstruct);
-		if (Result == 1)
-			return true;
-
-		return false;
-	}
-
-	@Override
-	public boolean InsertUSBInfo(USBInfo usb_info) throws SQLException {
-		boolean Result = false;
-
-		String SqlInstruct = "INSERT INTO auth_usb(id,usb_id,alias) VALUES ('" + usb_info.id + "','" + usb_info.usb_id
-				+ "','" + usb_info.alias + "')";
-		try {
-			Result = SearchUpdate(SqlInstruct);
-			if (Result == true)
-				return true;
-		} catch (SQLException se) {
-			// 이미 등록된 USB 정보가 있는 경우
-			if (se.getErrorCode() == 19) {
-				return false;
-			} else {
-				throw se;
-			}
-		}
-		return false;
-	}
-
-	@Override
-	public List<USBInfo> getRegisteredUSBList(String user) throws SQLException {
-		PreparedStatement pstmt = null;
-		String SqlInstruct = "SELECT * FROM auth_usb WHERE id='" + user + "'";
-		ResultSet rs = null;
-		List<USBInfo> usbList = new ArrayList<>();
-
-		pstmt = super.connection.prepareStatement(SqlInstruct);
-		rs = pstmt.executeQuery();
-
-		while (rs.next()) {
-			String id = rs.getString("id");
-			String usb_id = rs.getString("usb_id");
-			String alias = rs.getString("alias");
-
-			USBInfo data = new USBInfo();
-			data.id = id;
-			data.usb_id = usb_id;
-			data.alias = alias;
-
-			usbList.add(data);
-		}
-
-		return usbList;
-	}
-
->>>>>>> 51cf37ac16f4b1362ef8a6f6f23438bb1f9d8496
 	public boolean unregisterUSB(String user, String usb_id) {
 		String SqlInstruct = "DELETE FROM auth_usb WHERE id='" + user + "' AND usb_id='" + usb_id + "'";
 		try {
